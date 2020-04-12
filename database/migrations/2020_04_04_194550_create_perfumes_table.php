64ee8b92119b3162a2e5b6788fd8d478cd6912a4
Amaury Guillermin <amauryguillermin@gmail.com>
2020-04-12 20:13:15 +0200

    Migrations created

---
+++ database/migrations/2020_04_04_194550_create_perfumes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfumesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfumes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('fragrance')->nullable();
+            $table->integer('type', false, true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfumes');
+    }
+}
