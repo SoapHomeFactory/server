44b9dd35dca09306f910d31710f09527829eba71
Amaury Guillermin <amauryguillermin@gmail.com>
2020-04-14 22:26:45 +0200

    Fixing a (by mistake) erased seeder

---
+++ database/seeds/WaterSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WaterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('waters')->insert([
+          [
+            'name' => 'déminéralisée'
+          ],
+          [
+            'name' => 'ordinaire'
+          ]
+        ]);
+    }
+}
